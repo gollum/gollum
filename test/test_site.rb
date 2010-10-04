0b47bee9f035f2d1d01688721cf3b6fb58e2b14d
Daniel Reverri <reverri@gmail.com>
2010-10-03 23:17:17 -0700

    Changed site test to use it's own repo

--- test/test_site.rb
+++ test/test_site.rb
@@ -2,22 +2,19 @@ require File.join(File.dirname(__FILE__), *%w[helper])
 
 context "Site" do
   setup do
-    @wiki = Gollum::Wiki.new(testpath("examples/lotr.git"))
+    @wiki = Gollum::Wiki.new(testpath("examples/test_site.git"))
     @site = Gollum::Site.new(@wiki,
                              {:output_path => testpath("examples/site")})
   end
 
   test "generate static site" do
     @site.generate("master")
-    assert_equal(["/bilbo",
-                  "/Bilbo Baggins",
-                  "/Data.csv",
-                  "/Eye Of Sauron",
-                  "/Home",
-                  "/Mordor",
-                  "/Mordor/eye.jpg",
-                  "/Mordor/todo.txt",
-                  "/My Precious"],
+    assert_equal(["/Home",
+                  "/Page1",
+                  "/Page2",
+                  "/static",
+                  "/static/static.jpg",
+                  "/static/static.txt"],
                  Dir[@site.output_path + "/**/*"].map { |f| f.sub(@site.output_path, "") })
   end
 
