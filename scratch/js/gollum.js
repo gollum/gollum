67651afb48e00805c04a980c19e7372ed4e558be
Eston Bond <eston@github.com>
2010-10-25 16:57:33 -0700

    This should do

--- scratch/js/gollum.js
+++ scratch/js/gollum.js
@@ -1,5 +1,7 @@
 // ua
 $(document).ready(function() {
+  
+  // ua detection
   if ($.browser.mozilla) {
     $('body').addClass('ff');
   } else if ($.browser.webkit) {
@@ -12,4 +14,14 @@ $(document).ready(function() {
       $('body').addClass('ie8');
     }
   }
+  
+  // no widows in wiki body
+  if ($('#wiki-wrapper').hasClass('page')) {
+    $('#template h1, #template h2, #template h3, #template h4, #template h5, #template h6, #template li, #template p').each(
+        function(){
+            $(this).html($(this).html().replace(/\s([^\s<]+)\s*$/,'&nbsp;$1'));
+        }
+    );
+  }
+  
 });
