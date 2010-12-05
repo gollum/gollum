57d40c48e3b37769d08f5f72db445c73c454f875
Eston Bond <Eston Bond>
2010-12-04 16:20:49 -0800

    Add has_sidebar/has_footer to editor Get data-markup-lang attribute working dynamically

--- lib/gollum/frontend/views/create.rb
+++ lib/gollum/frontend/views/create.rb
@@ -16,6 +16,21 @@ module Precious
       def is_edit_page
         false
       end
+      
+      def format
+        @format = (@page.format || false) if @format.nil?
+        @format.to_s.downcase
+      end
+    
+      def has_footer 
+        @footer = (@page.footer || false) if @footer.nil?
+        !!@footer
+      end
+        
+      def has_sidebar
+        @sidebar = (@page.sidebar || false) if @sidebar.nil?
+        !!@sidebar
+      end
 
       def page_name
         @name.gsub('-', ' ')
--- lib/gollum/frontend/views/edit.rb
+++ lib/gollum/frontend/views/edit.rb
@@ -16,6 +16,21 @@ module Precious
       def is_edit_page
         true
       end
+        
+      def format
+        @format = (@page.format || false) if @format.nil?
+        @format.to_s.downcase
+      end
+    
+      def has_footer 
+        @footer = (@page.footer || false) if @footer.nil?
+        !!@footer
+      end
+        
+      def has_sidebar
+        @sidebar = (@page.sidebar || false) if @sidebar.nil?
+        !!@sidebar
+      end
 
       def title
         "#{@page.title}"
