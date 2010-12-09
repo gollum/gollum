8946d098f93abf9bfbe47b7995ac7faa361568c1
rick <technoweenie@gmail.com>
2010-12-09 13:31:51 -0800

    fix the page editor to show footer/sidebar content

--- lib/gollum/frontend/views/edit.rb
+++ lib/gollum/frontend/views/edit.rb
@@ -5,37 +5,48 @@ module Precious
 
       attr_reader :page, :content
 
+      def title
+        "#{@page.title}"
+      end
+
       def page_name
         @name.gsub('-', ' ')
+      end
+
+      def footer
+        if @footer.nil?
+          if page = @page.footer
+            @footer = page.raw_data
+          else
+            @footer = false
+          end
+        end
+        @footer
+      end
+
+      def sidebar
+        if @sidebar.nil?
+          if page = @page.sidebar
+            @sidebar = page.raw_data
+          else
+            @sidebar = false
+          end
+        end
+        @sidebar
       end
 
       def is_create_page
         false
       end
-      
+
       def is_edit_page
         true
       end
-        
+
       def format
         @format = (@page.format || false) if @format.nil?
         @format.to_s.downcase
       end
-    
-      def has_footer 
-        @footer = (@page.footer || false) if @footer.nil?
-        !!@footer
-      end
-        
-      def has_sidebar
-        @sidebar = (@page.sidebar || false) if @sidebar.nil?
-        !!@sidebar
-      end
-
-      def title
-        "#{@page.title}"
-      end
-      
     end
   end
 end
