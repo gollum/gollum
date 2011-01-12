2dbb4e9fa7d5b1f8175249a2f51c028332de1f4f
Douglas Campos <qmx@qmx.me>
2011-01-11 23:57:22 -0200

    fixed mediawiki link handling

--- test/test_markup.rb
+++ test/test_markup.rb
@@ -169,6 +169,13 @@ context "Markup" do
     assert_equal "<p>a <a class=\"internal present\" href=\"/Potato\">Potato Heaad</a></p>", output
   end
 
+  test "page link with different text on mediawiki" do
+    @wiki.write_page("Potato", :mediawiki, "a [[Potato|Potato Heaad]] ", commit_details)
+    page = @wiki.page("Potato")
+    output = page.formatted_data
+    assert_equal "<p>\na <a class=\"internal present\" href=\"/Potato\">Potato Heaad</a> \n</p>", output
+  end
+
   #########################################################################
   #
   # Images
