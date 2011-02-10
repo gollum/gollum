d76a40b1a82c56944ab575e6bb698195f405bf1e
Caleb Spare <cespare@gmail.com>
2011-02-09 16:33:51 -0800

    [Frontend] Small fix (no change in functionality) based on technoweenie's CR.

--- lib/gollum/frontend/app.rb
+++ lib/gollum/frontend/app.rb
@@ -57,7 +57,7 @@ module Precious
     post '/edit/*' do
       wiki = Gollum::Wiki.new(settings.gollum_path, settings.wiki_options)
       page = wiki.page(params[:splat].first)
-      name = params[:rename] || params[:page]
+      name = params[:rename] || page.name
       committer = Gollum::Committer.new(wiki, commit_message)
       commit    = {:committer => committer}
 
