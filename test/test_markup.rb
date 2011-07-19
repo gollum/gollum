aa258dbe6b802dce4171f493bfc76d56819457ad
Scott Chacon <schacon@gmail.com>
2011-07-19 12:55:43 -0700

    fix test breaking due to new footnote anchor

--- test/test_markup.rb
+++ test/test_markup.rb
@@ -476,8 +476,8 @@ context "Markup" do
   test "id prefix added" do
     content = "h2(#foo). xxxx[1]\n\nfn1.footnote"
     output = "<h2 id=\"wiki-foo\">xxxx" +
-             "<sup class=\"footnote\"><a href=\"#wiki-fn1\">1</a></sup></h2>" +
-             "\n<p class=\"footnote\" id=\"wiki-fn1\"><sup>1</sup> footnote</p>"
+             "<sup class=\"footnote\" id=\"wiki-fnr1\"><a href=\"#wiki-fn1\">1</a></sup></h2>" +
+             "\n<p class=\"footnote\" id=\"wiki-fn1\"><a href=\"#wiki-fnr1\"><sup>1</sup></a> footnote</p>"
     compare(content, output, :textile)
   end
 
