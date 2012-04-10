4480ff0520f01965d4cd3f7d11f56673234a371c
Corey Donohoe <atmos@atmos.org>
2012-04-10 15:55:30 -0700

    update the gemspec to reflect mathjax being gone

--- lib/gollum.rb
+++ lib/gollum.rb
@@ -20,7 +20,7 @@ require File.expand_path('../gollum/sanitization', __FILE__)
 require File.expand_path('../gollum/tex', __FILE__)
 
 module Gollum
-  VERSION = '1.3.1'
+  VERSION = '1.3.2'
 
   class Error < StandardError; end
 
