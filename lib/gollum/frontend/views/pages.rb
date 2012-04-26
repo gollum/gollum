3fc89cd36525934809a4e529a697a0406f9496f8
Darren Oakley <daz.oakley@gmail.com>
2012-04-26 16:52:25 +0100

    Make the /pages page look more like a filesystem

--- lib/gollum/frontend/views/pages.rb
+++ lib/gollum/frontend/views/pages.rb
@@ -7,6 +7,50 @@ module Precious
         "All pages in #{@ref}"
       end
 
+      def pages_as_nested_list
+        tree = {}
+        if has_results
+          @results.each do |page|
+            # "example/foo/bar"
+            #   => ["example", "foo", "bar"]
+            #     => tree["example"]["foo"]["bar"] = 'path'
+
+            # Split our path.
+            path_chunks = page.path.split('/')
+
+            # Create the root.
+            tree[path_chunks.first] ||= {}
+
+            # Keep track of current node.
+            node = tree[path_chunks.first]
+
+            # Deal with the children.
+            path_chunks[1..-1].each do |chunk|
+              if chunk == path_chunks.last
+                node[chunk] = page.path
+              end
+              node[chunk] ||= {}
+              node = node[chunk]
+            end
+          end
+
+          "<ul>#{listify(tree)}</ul>"
+        end
+      end
+
+      def listify(tree, path="")
+        html = ""
+        tree.each do |parent, child|
+          if !child.is_a? Hash
+            html << "<li><a href=\"/edit/#{child}\">#{parent}</a></li>"
+          else
+            html << "<li>#{parent}<ul>#{listify(child)}</ul></li>"
+          end
+        end
+
+        html
+      end
+
       def has_results
         !@results.empty?
       end
