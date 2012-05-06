872776660bc53b36f36723b227823d7bdcb0dab8
Corey Donohoe <atmos@atmos.org>
2012-05-06 12:08:12 -0700

    next version will be 2.0

--- lib/gollum.rb
+++ lib/gollum.rb
@@ -21,7 +21,7 @@ require File.expand_path('../gollum/tex', __FILE__)
 require File.expand_path('../gollum/web_sequence_diagram', __FILE__)
 
 module Gollum
-  VERSION = '1.4.3'
+  VERSION = '2.0.0'
 
   def self.assets_path
     ::File.expand_path('gollum/frontend/public', ::File.dirname(__FILE__))
