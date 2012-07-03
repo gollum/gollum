32930cee013a37b99d1fa9f21bcb00fd578a612e
bootstraponline <cafe@bootstraponline.com>
2012-07-03 10:55:42 -0600

    extract_path fails on nil paths.

--- lib/gollum/frontend/helpers.rb
+++ lib/gollum/frontend/helpers.rb
@@ -2,6 +2,7 @@ module Precious
   module Helpers
     # Extract the path string that Gollum::Wiki expects
     def extract_path(file_path)
+      return nil if file_path.nil?
       last_slash = file_path.rindex("/")
       if last_slash
         file_path[0, last_slash]
