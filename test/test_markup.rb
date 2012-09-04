427371af359f3b59a514599a12722d096df97166
Henrik <henrik@haf.se>
2012-09-04 22:57:39 +0200

    need some food now

--- test/test_markup.rb
+++ test/test_markup.rb
@@ -1,6 +1,6 @@
 # ~*~ encoding: utf-8 ~*~
 require File.expand_path( "../helper", __FILE__ )
-require File.expand_path( "wiki_factory", __FILE__ )
+require File.expand_path( "../wiki_factory", __FILE__ )
 
 context "Markup" do
   setup do
