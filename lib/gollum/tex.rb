9219a68971a33ccaca42dfd16694c43584068be1
Neal Pisenti (UMD) <npisenti@umd.edu>
2012-09-06 13:30:45 -0400

    fixed to_html method to uri-escape plus signs from TeX, to be passed to mathtran.org api

--- lib/gollum/tex.rb
+++ lib/gollum/tex.rb
@@ -7,7 +7,7 @@ module Gollum
     TEX_ENV = { :inline => lambda { |tex| "\\(#{tex}\\)" }, :block => lambda { |tex| "\\[#{tex}\\]" } }
 
     def self.to_html(tex, type = :inline)
-      tex_uri = EscapeUtils.escape_uri(tex)
+      tex_uri = EscapeUtils.escape_uri(tex).gsub(/\+/, '%2B')
       tex_alt = EscapeUtils.escape_html(tex)
       %{<img src="#{TEX_URL}?D=#{TEX_SIZES[type]};tex=#{tex_uri}" alt="#{tex_alt}">}
     end
