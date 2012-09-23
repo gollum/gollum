7ab9cafa793ee8c2c3f7da54881475b9d3c603b8
repotag <repotag@b6a9f8eb-fc23-4c85-89d7-9c1f5094cf3d>
2012-09-23 10:03:04 +0000

    Added fixtures directory, test repo and helper methods fixture and create_temp_repo. #bart
    git-svn-id: https://repotag.svn.beanstalkapp.com/repotag/gems/rjgit@42 b6a9f8eb-fc23-4c85-89d7-9c1f5094cf3d

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,21 @@
 require 'rjgit'
 include RJGit
 
-def valid_repo_attributes
-  {
-    :path => "/tmp/repo_test"
-  }
+  def valid_repo_attributes
+    {
+      :path => "/tmp/repo_test"
+    }
+  
+  def fixture(name)
+    File.read(File.join(File.dirname(__FILE__), 'fixtures', name))
+  end
+
+  def create_temp_repo(clone_path)
+    filename = 'git_test' + Time.now.to_i.to_s + rand(300).to_s.rjust(3, '0')
+    tmp_path = File.join("/tmp/", filename)
+    FileUtils.mkdir_p(tmp_path)
+    FileUtils.cp_r(clone_path, tmp_path)
+    File.join(tmp_path, 'dot_git')
+  end
+  
 end
