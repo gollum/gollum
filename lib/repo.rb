d31791b34468b83342900cfb2d5385fef42af6dc
repotag <repotag@b6a9f8eb-fc23-4c85-89d7-9c1f5094cf3d>
2012-09-23 13:15:15 +0000

    Implemented pecs for creating new repositories.
    git-svn-id: https://repotag.svn.beanstalkapp.com/repotag/gems/rjgit@60 b6a9f8eb-fc23-4c85-89d7-9c1f5094cf3d

--- lib/repo.rb
+++ lib/repo.rb
@@ -9,12 +9,11 @@ module RJGit
     attr_accessor :git
     attr_accessor :repo
     attr_accessor :path
-    attr_accessor :working_dir
-    attr_reader :bare
     
     def initialize(path, options = {}, create = false)
       epath = File.expand_path(path)
-
+      
+      bare = false
       if File.exist?(File.join(epath, '/.git'))
         bare = false
       elsif File.exist?(epath) || options[:bare]
@@ -22,10 +21,9 @@ module RJGit
       end
       
       @path = bare ? epath : File.join(epath, '/.git')
-      @working_dir = epath unless bare
       
       repo_path = java.io.File.new(@path)
-      @repo = bare ? RepositoryBuilder.new().set_bare.set_git_dir(repo_path).build() : RepositoryBuilder.new().set_work_tree(repo_path).build()
+      @repo = bare ? RepositoryBuilder.new().set_bare.set_git_dir(repo_path).build() : RepositoryBuilder.new().set_git_dir(repo_path).build()
       @repo.create(bare) if create
       @git = RubyGit.new(@repo)
     end
@@ -47,7 +45,12 @@ module RJGit
       @repo.get_branch
     end
     
+    def branches
+      return []
+    end
+    
     def description
+      return "blah"
     end
   
     def add(fpattern)
--- spec/repo_spec.rb
+++ spec/repo_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Repo do
     
   before(:all) do
+    @create_new = true
     @temp_repo_path = create_temp_repo(TEST_REPO_PATH)
     @temp_bare_repo_path = create_temp_repo(TEST_BARE_REPO_PATH)
     @repo = Repo.new(@temp_repo_path)
@@ -17,6 +18,20 @@ describe Repo do
     File.basename(@bare_repo.path).should_not eql ".git"
   end
   
+  it "should create a new repository if specified" do
+    filename = 'git_create_test' + Time.now.to_i.to_s + rand(300).to_s.rjust(3, '0')
+    tmp_path = File.join("/tmp/", filename)
+    new_repo = Repo.new(tmp_path, {:bare => false}, @create_new)
+    File.exists?(tmp_path + '/.git').should eql true
+  end
+
+  it "should create a new bare repository if specified" do
+    filename = 'git_create_bare_test' + Time.now.to_i.to_s + rand(300).to_s.rjust(3, '0')
+    tmp_path = File.join("/tmp/", filename)
+    new_bare_repo = Repo.new(tmp_path, {:bare => true}, @create_new)
+    File.directory?(tmp_path).should eql true
+  end
+  
   it "should tell us whether it is bare" do
     @repo.bare?.should eql false
     @bare_repo.bare?.should eql true
@@ -26,10 +41,14 @@ describe Repo do
     @repo.repo.kind_of?(org.eclipse.jgit.lib.Repository).should eql true 
   end
   
+  it "should list the current branch" do
+    @repo.branch.should eql "master"
+  end
+  
   it "should list its branches" do
     result = @repo.branches
     result.is_a?(Array).should eql true
-    result.include?("MASTER").should eql true
+    result.include?("master").should eql true
   end
   
   it "should return its description" do
