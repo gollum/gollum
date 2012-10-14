1b952b6d56c0a5904d67aa47b267a8c5510e3106
bootstraponline <cafe@bootstraponline.com>
2012-10-14 17:24:33 -0600

    Release 2.2.7

--- lib/gollum.rb
+++ lib/gollum.rb
@@ -23,7 +23,7 @@ require File.expand_path('../gollum/web_sequence_diagram', __FILE__)
 require File.expand_path('../gollum/frontend/uri_encode_component', __FILE__)
 
 module Gollum
-  VERSION = '2.2.6'
+  VERSION = '2.2.7'
 
   def self.assets_path
     ::File.expand_path('gollum/frontend/public', ::File.dirname(__FILE__))
