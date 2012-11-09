2783257f06df0d59908d491c03c4d182a48f8a82
Daniel Kimsey <dekimsey@ufl.edu>
2012-11-09 18:25:33 -0500

    Added page.meta_data to tests

--- test/test_git_access.rb
+++ test/test_git_access.rb
@@ -18,7 +18,7 @@ context "GitAccess" do
     assert @access.ref_map.empty?
     assert @access.tree_map.empty?
     @access.tree 'master'
-    assert_equal({"master"=>"629aa678272b017a4d136d35e77ac94d80b08dc2"}, @access.ref_map)
+    assert_equal({"master"=>"7d6aeab8b84c895f21f6c66b84a457b0fced9693"}, @access.ref_map)
 
     @access.tree '1db89ebba7e2c14d93b94ff98cfa3708a4f0d4e3'
     map = @access.tree_map['1db89ebba7e2c14d93b94ff98cfa3708a4f0d4e3']
