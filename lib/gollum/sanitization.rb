80088832b98b29273ee53013f3905a99196cb0c9
bootstraponline <code@bootstraponline.com>
2012-11-16 17:45:56 -0700

    Fix #587
    The default behavior prevented linking to header anchors. id_prefix
    can still be set to 'wiki-' however it is no longer the default.

--- lib/gollum/sanitization.rb
+++ lib/gollum/sanitization.rb
@@ -105,7 +105,7 @@ module Gollum
     attr_reader :transformers
 
     # Gets or sets a String prefix which is added to ID attributes.
-    # Default: 'wiki-'
+    # Default: ''
     attr_accessor :id_prefix
 
     # Gets a Hash describing HTML attributes that Sanitize should add.
@@ -128,7 +128,7 @@ module Gollum
       @add_attributes   = {}
       @remove_contents  = REMOVE_CONTENTS.dup
       @allow_comments   = false
-      @id_prefix        = 'wiki-'
+      @id_prefix        = ''
       yield self if block_given?
     end
 
