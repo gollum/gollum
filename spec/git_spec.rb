61734b4e8d8aa0d4cf083b9263b046982b095f8d
repotag <repotag@b6a9f8eb-fc23-4c85-89d7-9c1f5094cf3d>
2012-12-05 20:22:20 +0000

    Added new git specs that need implementing. #bart
    git-svn-id: https://repotag.svn.beanstalkapp.com/repotag/gems/rjgit@92 b6a9f8eb-fc23-4c85-89d7-9c1f5094cf3d

--- spec/git_spec.rb
+++ spec/git_spec.rb
@@ -2,6 +2,18 @@ require 'spec_helper'
 
 describe RubyGit do
   
+  it "should return log information"
+  
+  it "should tag with commit or revision id"
+  
+  it "should tag without commit or revision id"
+  
+  it "should clone repositories"
+  
+  it "should apply a patch to a file"
+  
+  it "should remove untracked files from the working tree with #clean"
+  
 end
 
 
