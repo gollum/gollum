176304ef1f2f8fb002ec6a8649918552a590b293
repotag <repotag@b6a9f8eb-fc23-4c85-89d7-9c1f5094cf3d>
2012-12-09 13:49:46 +0000

    Rewrote the commit.rb class to the new name-convention: jcommit is a Java commit, commit is a Ruby commit. #steven
    git-svn-id: https://repotag.svn.beanstalkapp.com/repotag/gems/rjgit@99 b6a9f8eb-fc23-4c85-89d7-9c1f5094cf3d

--- lib/commit.rb
+++ lib/commit.rb
@@ -13,32 +13,32 @@ module RJGit
     attr_reader :committed_date
     attr_reader :message
     attr_reader :short_message
-    attr_reader :revcommit
+    attr_reader :jcommit
     attr_reader :count
   
-    RJGit.delegate_to(RevCommit, :@revcommit)
+    RJGit.delegate_to(RevCommit, :@jcommit)
     
     def initialize(commit)
-      @revcommit = commit
+      @jcommit = commit
       @id = ObjectId.to_string(commit.get_id)
-      @actor = Actor.new(@revcommit.get_author_ident)
-      @committer = Actor.new(@revcommit.get_committer_ident)
-      @committed_date = Time.at(@revcommit.commit_time)
-      @message = @revcommit.get_full_message
-      @short_message = @revcommit.get_short_message
-      @count = @revcommit.get_parent_count
+      @actor = Actor.new(@jcommit.get_author_ident)
+      @committer = Actor.new(@jcommit.get_committer_ident)
+      @committed_date = Time.at(@jcommit.commit_time)
+      @message = @jcommit.get_full_message
+      @short_message = @jcommit.get_short_message
+      @count = @jcommit.get_parent_count
     end
   
     def parents
-      @parents ||= @revcommit.get_parents.map{|parent| Commit.new(parent) }
+      @parents ||= @jcommit.get_parents.map{|parent| Commit.new(parent) }
     end
     
     def self.find_all(repository, ref, options)
-      repository = repository_type(repository)
-      return nil if repository.nil?
+      jrepo = repository_type(repository)
+      return nil if jrepo.nil?
       begin
-        walk = RevWalk.new(repository);
-        objhead = repository.resolve(ref)
+        walk = RevWalk.new(jrepo);
+        objhead = jrepo.resolve(ref)
         root = walk.parse_commit(objhead)
         walk.mark_start(root)
         commits = walk.map { |commit| Commit.new(commit) }
