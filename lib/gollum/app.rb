a69d62911ca2310b77fcf390a2573e6f29a1e528
ngyuki <ngyuki.ts@gmail.com>
2013-08-09 14:45:19 +0900

    Fix editing of the sidebar with multi-byte characters.

--- lib/gollum/app.rb
+++ lib/gollum/app.rb
@@ -141,8 +141,7 @@ module Precious
         else
           @page = page
           @page.version = wiki.repo.log(wiki.ref, @page.path).first
-          raw_data = page.raw_data
-          @content = raw_data.respond_to?(:force_encoding) ? raw_data.force_encoding('UTF-8') : raw_data
+          @content = page.text_data
           mustache :edit
         end
       else
