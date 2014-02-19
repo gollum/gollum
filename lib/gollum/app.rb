e3d45894a92b6d580b8be49d649136cd5c0a50ad
alekhya <alekhya.rudra@cybrilla.com>
2014-02-19 16:08:49 +0530

    Implementing authentication on create, edit, delete, rename

--- lib/gollum/app.rb
+++ lib/gollum/app.rb
@@ -43,6 +43,22 @@ module Precious
     register Mustache::Sinatra
     include Precious::Helpers
 
+    helpers do
+
+      def protected!
+        unless authorized?
+          response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
+          throw(:halt, [401, "Not authorized\n"])
+        end
+      end
+
+      def authorized?
+        @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+        @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == ['admin', 'admin']
+      end
+
+    end
+
     dir = File.dirname(File.expand_path(__FILE__))
 
     # Detect unsupported browsers.
@@ -126,6 +142,7 @@ module Precious
     end
 
     get '/edit/*' do
+      protected!
       wikip = wiki_page(params[:splat].first)
       @name = wikip.name
       @path = wikip.path
@@ -197,6 +214,7 @@ module Precious
     end
 
     post '/rename/*' do
+      protected!
       wikip     = wiki_page(params[:splat].first)
       halt 500 if wikip.nil?
       wiki      = wikip.wiki
@@ -233,6 +251,7 @@ module Precious
     end
 
     post '/edit/*' do
+      protected!
       path      = '/' + clean_url(sanitize_empty_params(params[:path])).to_s
       page_name = CGI.unescape(params[:page])
       wiki      = wiki_new
@@ -251,6 +270,7 @@ module Precious
     end
 
     get '/delete/*' do
+      protected!
       wikip = wiki_page(params[:splat].first)
       name = wikip.name
       wiki = wikip.wiki
@@ -263,6 +283,7 @@ module Precious
     end
 
     get '/create/*' do
+      protected!
       wikip = wiki_page(params[:splat].first.gsub('+', '-'))
       @name = wikip.name.to_url
       @path = wikip.path
@@ -286,6 +307,7 @@ module Precious
     end
 
     post '/create' do
+      protected!
       name         = params[:page].to_url
       path         = sanitize_empty_params(params[:path]) || ''
       format       = params[:format].intern
