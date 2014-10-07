03a86b2bdd03cc3ae1606a4d0d4be1356b4398a6
Étienne Charignon <etienne.charignon@gmail.com>
2014-10-07 17:13:36 +0200

    latest_changes: handle renames a bit less brokenly

--- lib/gollum/views/latest_changes.rb
+++ lib/gollum/views/latest_changes.rb
@@ -21,10 +21,10 @@ module Precious
             :gravatar  => Digest::MD5.hexdigest(v.author.email.strip.downcase),
             :identicon => self._identicon_code(v.author.email),
             :date_full => v.authored_date,
-            :pages     => v.stats.files.map { |f,*rest|
-              path = f.gsub(/\..*$/, '')
-              { :path => path,
-                :path_with_commit => "#{path}/#{v.id}"
+            :files     => v.stats.files.map { |f,*rest|
+              page_path = f.gsub(/^.*=> /, '').gsub(/\..*$/, '')
+              { :file => f,
+                :link => "#{page_path}/#{v.id}"
               }
             }
           }
