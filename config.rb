0a9b35c0392980e7a0aecbe7107e318aef32f3f9
SkyCrawl <smolikj@e-trends.cz>
2015-08-12 22:26:20 +0200

    Restoring config files to repo root

---
+++ config.rb
@@ -0,0 +1,11 @@
+=begin
+This file can be used to (e.g.):
+- alter certain inner parts of Gollum,
+- extend it with your stuff.
+
+It is especially useful for customizing supported formats/markups. For more information and examples:
+- https://github.com/gollum/gollum#config-file
+
+=end
+
+# enter your Ruby code here ...
