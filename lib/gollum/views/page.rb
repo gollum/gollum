4c70e96bf842912bf66f0eb104e887a78bcc889e
Victor Bogado <victor@bogado.net>
2016-12-31 12:56:17 -0800

    Remove logic to deal with "h1_title".
    This logic was moved into gollum-lib where the Page object now knows how
    to create it's title.

--- lib/gollum/views/page.rb
+++ lib/gollum/views/page.rb
@@ -6,19 +6,13 @@ module Precious
       attr_reader :content, :page, :header, :footer
       DATE_FORMAT    = "%Y-%m-%d %H:%M:%S"
       DEFAULT_AUTHOR = 'you'
-      @@to_xml       = { :save_with => Nokogiri::XML::Node::SaveOptions::DEFAULT_XHTML ^ 1, :indent => 0, :encoding => 'UTF-8' }
 
       def title
-        h1 = @h1_title ? page_header_from_content(@content) : false
-        h1 || @page.url_path_title
+          @page.title
       end
 
       def page_header
         title
-      end
-
-      def content
-        content_without_page_header(@content)
       end
 
       def author
@@ -141,52 +135,6 @@ module Precious
       # Returns Hash.
       def metadata
         @page.metadata
-      end
-
-      private
-
-      # Wraps page formatted data to Nokogiri::HTML document.
-      #
-      def build_document(content)
-        Nokogiri::HTML::fragment(%{<div id="gollum-root">} + content.to_s + %{</div>}, 'UTF-8')
-      end
-
-      # Finds header node inside Nokogiri::HTML document.
-      #
-      def find_header_node(doc)
-        case @page.format
-          when :asciidoc
-            doc.css("div#gollum-root > h1:first-child")
-          when :org
-            doc.css("div#gollum-root > p.title:first-child")
-          when :pod
-            doc.css("div#gollum-root > a.dummyTopAnchor:first-child + h1")
-          when :rest
-            doc.css("div#gollum-root > div > div > h1:first-child")
-          else
-            doc.css("div#gollum-root > h1:first-child")
-        end
-      end
-
-      # Extracts title from page if present.
-      #
-      def page_header_from_content(content)
-        doc   = build_document(content)
-        title = find_header_node(doc).inner_text.strip
-        title = nil if title.empty?
-        title
-      end
-
-      # Returns page content without title if it was extracted.
-      #
-      def content_without_page_header(content)
-        doc = build_document(content)
-          if @h1_title
-            title = find_header_node(doc)
-            title.remove unless title.empty?
-          end
-        # .inner_html will cause href escaping on UTF-8
-        doc.css("div#gollum-root").children.to_xml(@@to_xml)
       end
     end
   end
