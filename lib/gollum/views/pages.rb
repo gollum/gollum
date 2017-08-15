a462a924f75d78a5559f54a66b8a2d8b5d5a4a55
Dawa Ometto <dawa.ometto@phil.uu.nl>
2017-08-15 13:50:18 +0200

    Fix helper and path helper

--- lib/gollum/views/pages.rb
+++ lib/gollum/views/pages.rb
@@ -41,7 +41,7 @@ module Precious
             if page_path.include?('/')
               folder      = page_path.split('/').first
               folder_path = @path ? "#{@path}/#{folder}" : folder
-              folder_link = %{<li><a href="#{@pages_path}/#{folder_path}/" class="folder">#{folder}</a></li>}
+              folder_link = %{<li><a href="#{pages_path}/#{folder_path}/" class="folder">#{folder}</a></li>}
 
               folders[folder] = folder_link unless folders.key?(folder)
             elsif page_path != ".gitkeep"
