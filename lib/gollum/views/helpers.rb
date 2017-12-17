390d63e561e94b7a7faa2803110f357c8812dddc
Dawa Ometto <dawa.ometto@phil.uu.nl>
2017-12-17 19:51:25 +0100

    Refactored sprockets helpers for mustache

--- lib/gollum/views/helpers.rb
+++ lib/gollum/views/helpers.rb
@@ -42,28 +42,27 @@ module Precious
     module SprocketsHelpers
       def self.included(base)
 
-        def helper_proc_with_options(method)
-          Proc.new do |args|
+        def sprockets_stylesheet_tag
+          puts "STYLESHEET TAG"
+          lambda do |args|
             args = args.split(' ')
-            if args.size > 1 then
-              options = args[1..-1].join(' ')
-              options = YAML.safe_load("---\n#{options}\n", [Symbol])
-            end
-            options = options.respond_to?(:to_h) ? options.to_h : {}
-            options = options.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
-            send(method, args[0], options)
-          end  
-        end
-
-        ['stylesheet_path','javascript_path', 'image_path'].each do |method|
-          define_method :"#{method}_mustache" do
-            Proc.new {|args| send(method.to_sym, args)}
+            name = args[0]
+            options = {:media => :all}
+            options[:media] = :print if args[1] == 'print'
+            puts "FOUND: " + name.inspect
+            send(:stylesheet_tag, name, options)
           end
         end
 
-        ['stylesheet_tag','javascript_tag'].each do |method|
-          define_method :"#{method}_mustache" do
-            helper_proc_with_options(method.to_sym)
+        def sprockets_javascript_tag
+          lambda do |name|
+            send(:javascript_tag, name)
+          end
+        end
+
+        def sprockets_image_path
+          lambda do |args|
+            send(:image_path, name)
           end
         end
       end
