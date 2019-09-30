7b66ab10a3c37fcba411b4272603a781ee114230
Dawa Ometto <dawa.ometto@phil.uu.nl>
2019-09-30 12:59:16 +0200

    Revert "WIP: Pagination"
    This reverts commit 4d627bf6c5d9a8d1b0194718c080630fe11d4911.

--- lib/gollum/views/search.rb
+++ lib/gollum/views/search.rb
@@ -1,29 +1,10 @@
 module Precious
   module Views
     class Search < Layout
-      attr_reader :query, :search_terms
+      attr_reader :results, :query, :search_terms
 
       def title
         "Search results for " + @query
-      end
-
-      def sorted_results
-        sorted = @results.sort do |a, b|
-          if b.nil?
-            b_filename_count = 0
-            b_count          = 0
-          else
-            b_filename_count = b[:filename_count]
-            b_count          = b[:count]
-          end
-          [a[:filename_count], a[:count]] <=> [b_filename_count, b_count]
-        end
-        sorted.reverse!
-        sorted = sorted.each_slice(5).to_a # Paginate: groups of five
-        sorted = sorted.each_with_index.map do |result, index|
-          {:index => index.to_i + 1, :results => result}
-        end
-        sorted
       end
 
       def search
