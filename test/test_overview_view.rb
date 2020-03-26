878d33af9d1e1e62d82c47d773e4c4d5b24fbcc9
Dawa Ometto <dawa.ometto@phil.uu.nl>
2020-03-26 18:35:51 +0100

    Remove debug statement

--- test/test_overview_view.rb
+++ test/test_overview_view.rb
@@ -41,7 +41,6 @@ context "Precious::Views::Overview" do
   test "breadcrumb" do
     @page.instance_variable_set("@path", "Mordor/Eye-Of-Sauron/Saruman")
     @page.instance_variable_set("@base_url", "")
-    $stderr.puts @page.breadcrumb.inspect
     assert_equal "<nav aria-label=\"Breadcrumb\"><ol><li class=\"breadcrumb-item\"><a href=\"/gollum/overview\">Home</a></li>\n<li class=\"breadcrumb-item\"><a href=\"/gollum/overview/Mordor/\">Mordor</a></li>\n<li class=\"breadcrumb-item\"><a href=\"/gollum/overview/Mordor/Eye-Of-Sauron/\">Eye-Of-Sauron</a></li>\n<li class=\"breadcrumb-item\" aria-current=\"page\">Saruman</li>\n</ol></nav>", @page.breadcrumb
   end
 
