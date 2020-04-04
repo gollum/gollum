942199c269dc0f6faed8dfb7c5c28bf6b100213a
Dawa Ometto <dawa.ometto@phil.uu.nl>
2020-04-04 12:04:25 +0200

    Fix migration script when using page-file-dir and add test

--- test/test_migrate.rb
+++ test/test_migrate.rb
@@ -23,7 +23,26 @@ waa
 [[Subsub/Zaa.md]]
 EOF
 
-script_path = File.expand_path(File.join(File.dirname(__FILE__), '../', 'bin', 'gollum-migrate-tags'))
+def load_script(**args)
+  settings = {
+    :run_silent => true,
+    :no_dry_run => true,
+    :prefer_relative => true,
+    :hyphenate => false,
+    :page_file_dir => nil,
+  }.merge(args)
+  
+  settings.each do |const, val|
+    const_name = const.to_s.upcase
+    Object.const_set(const_name, val) unless Object.const_defined?(const_name) && Object.const_get(const_name) == val
+  end
+  
+  script_path = File.expand_path(File.join(File.dirname(__FILE__), '../', 'bin', 'gollum-migrate-tags'))
+  
+  Dir.chdir(@path) do
+    load script_path
+  end  
+end
 
 unless ENV['TRAVIS']
 
@@ -35,32 +54,29 @@ unless ENV['TRAVIS']
     end
     
     test 'repair broken links' do
-      PREFER_RELATIVE = true
-      RUN_SILENT = true
-      NO_DRY_RUN = true
-      HYPHENATE = false
-      
-      Dir.chdir(@path) do
-        load script_path
-      end
+      load_script
+    
       f = ::File.new(::File.join(@path, 'Subdir/Foo.md'), 'r')
       assert_equal result, f.read
     end
     
-    test 'change spaced filenames to hyphenated filenames' do
-      RUN_SILENT = true
-      NO_DRY_RUN = true
-      PREFER_RELATIVE = true
-      HYPHENATE = true
-    
-      Dir.chdir(@path) do
-        load script_path
-      end
+    test 'change spaced filenames to hyphenated filenames' do    
+      load_script(hyphenate: true)
     
       f = ::File.new(::File.join(@path, 'Home.textile'), 'r')
       output = f.read
       assert_equal true, output.include?('[[Bilbo-Baggins.md]]')
       assert_equal true, output.include?('[[evil|Mordor/Eye-Of-Sauron.md]]')
+    end
+    
+    test 'migration with page file dir' do      
+      load_script(page_file_dir: 'Subdir')
+      
+      f = ::File.new(::File.join(@path, 'Subdir/Foo.md'), 'r')
+      output = f.read
+      puts output.inspect
+      assert_equal true, output.include?('[[Subsub/Zaa.md]]')
+      assert_equal true, output.include?('[[Samwi]]')
     end
 
     teardown do
