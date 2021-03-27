37d805e75e54a22ef585678009c98c9a262a9563
Dawa Ometto <dawa.ometto@uni-leipzig.de>
2021-03-27 16:24:04 +0100

    Add integration tests with capybara

---
+++ test/integration/test_js_errors.rb
@@ -0,0 +1,43 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'helper'))
+require 'selenium-webdriver'
+require 'capybara'
+require 'capybara/dsl'
+
+def console_logs(page, level = :severe, &block)
+  yield page.driver.browser.manage.logs.get(:browser).select{|log| log.level == level.to_s.upcase }
+end
+
+def expected_errors
+  [
+    %r{Refused to apply style from 'http:.*/gollum/create/custom.css'}
+  ]
+end
+
+context 'Frontend with matjax' do
+  include Capybara::DSL
+  
+  setup do
+    @path = cloned_testpath("examples/lotr.git")
+    @wiki = Gollum::Wiki.new(@path)
+    Precious::App.set(:gollum_path, @path)
+    Precious::App.set(:wiki_options, {mathjax: true})
+    Capybara.app = Precious::App
+    Capybara.default_driver = :selenium_chrome
+    Capybara.server = :webrick
+  end
+  
+  test 'expected asset errors' do
+    visit '/'
+    console_logs(page) do |logs|
+      expected_errors.each do |error|
+        assert logs.find {|log| log.message.match?(error) }
+      end
+      assert logs.size == expected_errors.size # No other errors
+    end
+  end
+  
+  teardown do
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
+  end
+end
