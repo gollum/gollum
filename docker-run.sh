589b4bce398055d625bddd4443233b424148bfc8
James Reynolds <magnusviri@users.noreply.github.com>
2021-12-30 15:27:20 -0700

    Added docker environment variables for author and email (#1789)
    Sets git user.name and user.email if env var is set

--- docker-run.sh
+++ docker-run.sh
@@ -5,6 +5,14 @@ if [ ! -d .git ]; then
     git init
 fi
 
+# Set git user.name and user.email
+if [ ${GOLLUM_AUTHOR_USERNAME:+1} ]; then
+	git config user.name "${GOLLUM_AUTHOR_USERNAME}"
+fi
+if [ ${GOLLUM_AUTHOR_EMAIL:+1} ]; then
+	git config user.email "${GOLLUM_AUTHOR_EMAIL}"
+fi
+
 # Start gollum service
 [[ "$@" != *--mathjax* ]] && echo "WARNING: Mathjax will soon be disabled by default. To explicitly enable it, use --mathjax" >&2
 exec gollum $@ --mathjax
