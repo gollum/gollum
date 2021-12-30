e60d4e397c28a1d094d8737fa6a64495f3003b2b
benjamin wil <bw@benjaminwil.info>
2021-12-30 15:31:57 -0800

    Internationalize `Views::Compare` templates

--- lib/gollum/views/compare.rb
+++ lib/gollum/views/compare.rb
@@ -6,7 +6,7 @@ module Precious
       attr_reader :page, :diff, :versions, :message, :allow_editing
 
       def title
-        "Comparison of #{@page.title}"
+        [t[:comparison_of], @page.title].join(" ")
       end
 
       def before
