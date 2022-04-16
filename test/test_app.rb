6810211a3547ccbaef078f3973b2338612791cec
Tevin Zhang <mail2tevin@gmail.com>
2022-04-16 14:46:28 +0800

    Add a test for template filter with page parameter

--- test/test_app.rb
+++ test/test_app.rb
@@ -364,6 +364,25 @@ EOF
     Precious::App.set(:wiki_options, { :template_page => false })
   end
 
+  test "create with template filter with parameter succeed" do
+    Precious::App.set(:wiki_options, { :template_page => true })
+
+    # arrange
+    Gollum::TemplateFilter.add_filter("{{page_name}}", & -> (page) { page.name })
+    template_content = "# Daily Log, {{page_name}}"
+
+    @wiki.write_page("daily-logs/_Template",
+                     :markdown,
+                     template_content)
+    # act
+    get "/gollum/create/daily-logs/2022-04-16"
+    # assert
+    assert last_response.ok?
+    assert_match("# Daily Log, 2022-04-16", last_response.body)
+
+    Precious::App.set(:wiki_options, { :template_page => false })
+  end
+
   test "edit returns nil for non-existant page" do
     # post '/edit' fails. post '/edit/' works.
     page = 'not-real-page'
