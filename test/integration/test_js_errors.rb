2dc56258ccc63f5c97a17597eaeb14a933ee92f4
Dawa Ometto <dawa.ometto@uni-leipzig.de>
2022-06-19 00:00:20 +0200

    Only run Selenium tests conditional on CI flag

--- test/integration/test_js_errors.rb
+++ test/integration/test_js_errors.rb
@@ -1,48 +1,51 @@
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'helper'))
-require 'selenium-webdriver'
-require 'capybara/dsl'
+if ENV['CI'] || ENV['CAPYBARA'] then
+  require File.expand_path(File.join(File.dirname(__FILE__), '..', 'helper'))
+  require 'selenium-webdriver'
+  require 'capybara/dsl'
 
-def console_log(page, level = :severe)
-  page.driver.browser.manage.logs.get(:browser).select{|log| log.level == level.to_s.upcase }
-end
+  Capybara.default_driver = ::CAPYBARA_DRIVER
+  Capybara.server = :webrick
 
-def expected_errors
-  Regexp.union([
-    %r{Refused to apply style from 'http:.*/gollum/create/custom.css'}
-  ])
-end
-
-def assert_only_expected_errors(log)
-  assert_equal [], log.reject {|err| err.message.match?(expected_errors) }
-end
-
-context 'Frontend with mathjax' do
-  include Capybara::DSL
-  
-  setup do
-    @path = cloned_testpath("examples/lotr.git")
-    @wiki = Gollum::Wiki.new(@path)
-    Precious::App.set(:gollum_path, @path)
-    Precious::App.set(:wiki_options, {mathjax: true})
-    Capybara.app = Precious::App
-    Capybara.default_driver = :selenium_chrome
-    Capybara.server = :webrick
+  def console_log(page, level = :severe)
+    page.driver.browser.logs.get(:browser).select{|log| log.level == level.to_s.upcase }
   end
-  
-  test 'no unexpected errors on /' do
-    visit '/'
-    log = console_log(page)
-    assert_only_expected_errors(log)
+
+  def expected_errors
+    Regexp.union([
+      %r{Refused to apply style from 'http:.*/gollum/create/custom.css'}
+    ])
   end
-  
-  test 'no unexpected errors on /create/' do
-    visit '/create/Foobar'
-    log = console_log(page)
-    assert_only_expected_errors(log)
+
+  def assert_only_expected_errors(log)
+    assert_equal [], log.reject {|err| err.message.match?(expected_errors) }
   end
-  
-  teardown do
-    Capybara.reset_sessions!
-    Capybara.use_default_driver
+
+  context 'Frontend with mathjax' do
+    include Capybara::DSL
+    
+    setup do
+      @path = cloned_testpath("examples/lotr.git")
+      @wiki = Gollum::Wiki.new(@path)
+      Precious::App.set(:gollum_path, @path)
+      Precious::App.set(:wiki_options, {mathjax: true})
+      Capybara.app = Precious::App
+    end
+    
+    test 'no unexpected errors on /' do
+      visit '/'
+      log = console_log(page)
+      assert_only_expected_errors(log)
+    end
+    
+    test 'no unexpected errors on /create/' do
+      visit '/create/Foobar'
+      log = console_log(page)
+      assert_only_expected_errors(log)
+    end
+    
+    teardown do
+      Capybara.reset_sessions!
+      Capybara.use_default_driver
+    end
   end
 end
