8dcc58c0124c6dd37bd817d2d8949fd7dafa2113
Juan Rodenas <95724863+JuanRodenas@users.noreply.github.com>
2022-11-14 23:31:19 +0100

    Update config.rb
    Remove REDIS

--- config.rb
+++ config.rb
@@ -9,9 +9,6 @@ It is especially useful for customizing supported formats/markups. For more info
 =end
 
 # enter your Ruby code here ...
-  # Use Redis store for caching.
-  config.cache_store = :redis_cache_store, { url: ENV.fetch("REDIS_URL", "redis://localhost:6379/0") }
-  
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
