0f391d1b52a1625c1def78664441cca611b9f72b
Dawa Ometto <dawa.ometto@uni-leipzig.de>
2022-11-16 14:53:34 +0100

    Add link to sample config.rb (#1879)

--- config.rb
+++ config.rb
@@ -5,6 +5,7 @@ This file can be used to (e.g.):
 
 It is especially useful for customizing supported formats/markups. For more information and examples:
 - https://github.com/gollum/gollum#config-file
+- https://github.com/gollum/gollum/wiki/Sample-config.rb
 
 =end
 
