7e25da70598d9b6cb14e1c4cdca7cd88b8723c82
Dawa Ometto <dawa.ometto@uni-leipzig.de>
2022-12-20 15:34:34 +0100

    Move uglifier to development dependencies

--- lib/gollum/assets.rb
+++ lib/gollum/assets.rb
@@ -4,7 +4,6 @@ module Precious
   module Assets
     MANIFEST = %w(app.js editor.js mermaid.js app.css criticmarkup.css fileview.css ie7.css print.css *.png *.jpg *.svg *.eot *.ttf)
     ASSET_URL = 'gollum/assets'
-    JS_COMPRESSOR = :uglify unless defined?(JS_COMPRESSOR)
 
     def self.sprockets(dir = File.dirname(File.expand_path(__FILE__)))
       env = Sprockets::Environment.new
@@ -17,7 +16,7 @@ module Precious
       env.append_path ::File.join(dir, 'public/gollum/images')
       env.append_path ::File.join(dir, 'public/gollum/fonts')
 
-      env.js_compressor  = Precious::Assets::JS_COMPRESSOR unless Precious::App.development?
+      env.js_compressor  = ::Precious::Assets::JS_COMPRESSOR if defined?(::Precious::Assets::JS_COMPRESSOR)
       env.css_compressor = :scss
 
       env.context_class.class_eval do
