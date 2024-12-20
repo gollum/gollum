9324df2037745006404e95eff3c932dd7acf8ae4
Hooverdan96 <35113775+Hooverdan96@users.noreply.github.com>
2024-12-19 16:41:09 -0800

    Insert wiki permission check

--- docker-run.sh
+++ docker-run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Check if /wiki directory exists and is writable
+if [ ! -d "/wiki" ] || [ ! -w "/wiki" ]; then
+  echo "Error: /wiki directory does not exist or is not writable. Adjust permissions to mapped host volume. Exiting ..."
+  exit 1
+else
+  echo "The /wiki directory exists and is writable. Continuing ..."
+fi
+
 # Initialize the wiki
 if [ ! -d .git ] && [ "$(git rev-parse  --is-bare-repository 2>/dev/null)" != "true" ]; then
     git init
