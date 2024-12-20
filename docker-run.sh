94a9cf62deb615af4c75fd4d5b4d5f390c6cde06
Hooverdan96 <35113775+Hooverdan96@users.noreply.github.com>
2024-12-20 07:46:05 -0800

    Change permission test to warning & continue

--- docker-run.sh
+++ docker-run.sh
@@ -2,10 +2,9 @@
 
 # Check if /wiki directory exists and is writable
 if [ ! -d "/wiki" ] || [ ! -w "/wiki" ]; then
-  echo "Error: /wiki directory does not exist or is not writable. Adjust permissions to mapped host volume. Exiting ..."
-  exit 1
+  echo "Warning: /wiki directory does not exist or is not writable. Adjust permissions to mapped host volume."
 else
-  echo "The /wiki directory exists and is writable. Continuing ..."
+  echo "The /wiki directory exists and is writable."
 fi
 
 # Initialize the wiki
